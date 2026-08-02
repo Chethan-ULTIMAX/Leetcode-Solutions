32551affd498c43b6b4dce709dc2f608962d7a65
Chethan R <chethu05102009@gmail.com>
2026-08-02 10:08:28 +0530

    Add solution for Stone Game problem
    Implement the Stone Game solution with a mathematical observation that guarantees the first player wins when the number of piles is even.

---
+++ Game_Theory/877_Stone_Game.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Stone Game
+ * LeetCode: 877
+ *
+ * Approach: Mathematical Observation
+ * ----------------------------------
+ * For an even number of piles, the first
+ * player (Alice) can always guarantee a win.
+ *
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
